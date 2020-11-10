2e80bed6f3ad0203c5a679a147bd102eaba2500d
Willi Lazarov <lazarov.w@gmail.com>
2020-11-10 14:05:35 +0100

    Added searching through Exploit class

--- vulnscan.py
+++ vulnscan.py
@@ -7,13 +7,14 @@ api = shodan.Shodan(SHODAN_API_KEY)
 # Wrap the request in a try/ except block to catch errors
 try:
     # Search Shodan
-    results = api.search('apache')
+    results = api.exploits.search('SQLi Apache', page=1)
 
     # Show the results
     print('Results found: {}'.format(results['total']))
     for result in results['matches']:
-        print('IP: {}'.format(result['ip_str']))
-        print(result['data'])
+        print('Exploit ID: {}'.format(result['_id']))
+        print('Source: {}'.format(result['source']))
+        print('Description: {}'.format(result['description']))
         print('')
 except shodan.APIError as e:
     print('Error: {}'.format(e))
