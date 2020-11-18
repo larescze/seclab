96e02bebc1fc612c8fe8d7c06765631a28366b80
Willi Lazarov <lazarov.w@gmail.com>
2020-11-18 19:48:15 +0100

    Web App (testing version)

--- www/www/wsgi.py
+++ www/www/wsgi.py
@@ -2,9 +2,6 @@
 WSGI config for www project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
