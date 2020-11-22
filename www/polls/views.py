2cc4421c1f430db8ebc4aef0ff80dd9f6287ad4a
Willi Lazarov <lazarov.w@gmail.com>
2020-11-22 20:17:31 +0100

    Full search query

--- www/polls/views.py
+++ www/polls/views.py
@@ -1,8 +1,21 @@
 from .exploits import vulnscan
 from django.shortcuts import render
 
+from .forms import SearchForm
+
 
 def index(request):
     title = "Seclab"
-    search_results = vulnscan.search()
-    return render(request, 'index.html', {'title': title, 'results': search_results})
+    query = ""
+    search_results = ""
+    facets = []
+    if request.method == 'POST':
+        form = SearchForm(request.POST)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            facets = request.POST.getlist('facet')
+            search_results = vulnscan.search(query, facets)
+    else:
+        form = SearchForm()
+    return render(request, 'index.html',
+                  {'title': title, 'form': form, 'search_query': query, 'results': search_results, 'facets': facets})
