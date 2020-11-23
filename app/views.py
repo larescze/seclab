85512c45e5dbdbee3ca00fc5cadf767750dcc282
Willi Lazarov <lazarov.w@gmail.com>
2020-11-23 20:34:39 +0100

    Added advanced search

--- app/views.py
+++ app/views.py
@@ -9,13 +9,35 @@ def index(request):
     query = ""
     search_results = ""
     facets = []
+    chart = ""
+    page = 1
+    limit = 18
+    limit_val = ""
     if request.method == 'POST':
         form = SearchForm(request.POST)
         if form.is_valid():
             query = form.cleaned_data['query']
             facets = request.POST.getlist('facet')
-            search_results = vulnscan.search(query, facets)
+            chart = request.POST['chart']
+            page = int(request.POST['page'])
+            if request.POST['limit']:
+                limit = int(request.POST['limit'])
+                limit_val = limit
+            search_results = vulnscan.search(query, facets, page, limit)
     else:
         form = SearchForm()
-    return render(request, 'index.html',
-                  {'title': title, 'form': form, 'search_query': query, 'results': search_results, 'facets': facets})
+    return render(
+        request,
+        'index.html',
+        {
+            'title': title,
+            'form': form,
+            'search_query': query,
+            'results': search_results,
+            'facets': facets,
+            'limit': limit,
+            'limit_val': limit_val,
+            'chart': chart,
+            'page': page
+        }
+    )
--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -3,18 +3,43 @@ $(document).ready(function () {
     $("#searchIcon").replaceWith($("#searchGif"));
   });
 
+  $("#next").click(function () {
+    var page = parseInt($("#page").val()) + 1;
+    $("#page").val(page);
+    $("#searchForm").submit();
+  });
+
+  $("#previous").click(function () {
+    var page = parseInt($("#page").val()) - 1;
+    $("#page").val(page);
+    $("#searchForm").submit();
+  });
+
   $(".checked").each(function () {
     $(this).children(".form-check-input").prop("checked", true);
   });
 
+  $(".selected").prop("selected", true);
+
   var chartColors = ["#2382bc", "#b7ceeb", "#ff8b0a", "#a2e395", "#32aa32"];
 
   $(".btn-chart").on("click", function () {
-    $("#chartTitle").text($(this).siblings().find(".host").text());
+    var title = "Chart " + $(this).siblings().find(".host").text();
+    $("#chartTitle").text(title);
+
+    var charType = $("#chartType").text();
+
+    var isLegend = true;
+    if (charType === "bar") {
+      isLegend = false;
+    }
+
     var ctx = document.getElementById("summaryChart");
-    if (window.chart != undefined) {
+
+    if (window.chart instanceof Chart) {
       window.chart.destroy();
     }
+
     var label = [];
     $(this)
       .siblings()
@@ -22,6 +47,7 @@ $(document).ready(function () {
       .each(function () {
         label.push($(this).text());
       });
+
     var count = [];
     $(this)
       .siblings()
@@ -29,8 +55,9 @@ $(document).ready(function () {
       .each(function () {
         count.push($(this).text());
       });
+
     window.chart = new Chart(ctx, {
-      type: "pie",
+      type: charType,
       data: {
         labels: label,
         datasets: [
@@ -40,6 +67,11 @@ $(document).ready(function () {
             borderWidth: 1,
           },
         ],
+      },
+      options: {
+        legend: {
+          display: isLegend,
+        },
       },
     });
   });
