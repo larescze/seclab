542ea5734bbec88f2985e350cb976b9427690b45
Willi Lazarov <lazarov.w@gmail.com>
2020-12-01 21:29:06 +0100

    Added DoS

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponseRedirect
 
-from .exploits import vulnscan, xss, sqli
+from .exploits import vulnscan, xss, dos, sqli
 from django.shortcuts import render
 
 from .forms import SearchForm
@@ -50,20 +50,33 @@ def exploits(request):
         if request.method == 'POST':
             if request.POST.get('select-xss'):
                 selected = "xss"
-            if request.POST.get('select-sqli'):
+            elif request.POST.get('select-sqli'):
                 selected = "sqli"
-            if request.POST.get('sqli'):
-                selected = "sqli"
-                url = request.POST['url']
-                attack_type = request.POST['attack_type']
-                custom_code = request.POST['custom_code']
-                response = sqli.launch(url, attack_type, custom_code)
-            if request.POST.get('xss'):
+            elif request.POST.get('select-dos'):
+                selected = "dos"
+            elif request.POST.get('xss'):
                 selected = "xss"
                 url = request.POST['url']
                 attack_type = request.POST['attack_type']
                 custom_code = request.POST['custom_code']
                 response = xss.launch(url, attack_type, custom_code)
+            elif request.POST.get('dos'):
+                selected = "dos"
+                port = 80
+                socket_limit = 200
+                host = request.POST['host']
+                if request.POST['port']:
+                    port = int(request.POST['port'])
+                if request.POST['socket_limit']:
+                    socket_limit = int(request.POST['socket_limit'])
+                is_https = request.POST['is_https']
+                response = dos.launch(host, port, socket_limit, is_https)
+            elif request.POST.get('sqli'):
+                selected = "sqli"
+                url = request.POST['url']
+                attack_type = request.POST['attack_type']
+                custom_code = request.POST['custom_code']
+                response = sqli.launch(url, attack_type, custom_code)
         return render(request, 'exploits.html', {'title': title, 'selected': selected, 'response': response})
     else:
         return HttpResponseRedirect('/admin/login/')
