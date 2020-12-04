6b305823825393660dff3bee1a316de0dda678a3
Willi Lazarov <lazarov.w@gmail.com>
2020-12-04 13:57:28 +0100

    SK

--- seclab/settings.py
+++ seclab/settings.py
@@ -3,9 +3,13 @@ Django settings for Seclab project.
 """
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# SK
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
